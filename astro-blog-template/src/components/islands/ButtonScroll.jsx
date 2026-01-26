52540d6b6bcfac92575ce74ad905ae269e07a9fa
jonatan villalva <jvillalva.sistemas@gmail.com>
2026-01-26 20:58:38 -0300

    Resetting header. Initial hero development.

--- astro-blog-template/src/components/islands/ButtonScroll.jsx
+++ astro-blog-template/src/components/islands/ButtonScroll.jsx
@@ -1,37 +1,37 @@
-import { useState, useEffect } from "preact/hooks";
-import ArrowTopIcon from "../icons/ArrowTopIcon";
+// import { useState, useEffect } from "preact/hooks";
+// import ArrowTopIcon from "../icons/ArrowTopIcon";
 
-export default function ScrollToTop() {
-  const [isVisible, setIsVisible] = useState(false);
+// export default function ScrollToTop() {
+//   const [isVisible, setIsVisible] = useState(false);
 
-  useEffect(() => {
-    const toggleVisibility = () => {
-      setIsVisible(window.scrollY > 300);
-    };
+//   useEffect(() => {
+//     const toggleVisibility = () => {
+//       setIsVisible(window.scrollY > 300);
+//     };
 
-    window.addEventListener("scroll", toggleVisibility);
+//     window.addEventListener("scroll", toggleVisibility);
 
-    return () => {
-      window.removeEventListener("scroll", toggleVisibility);
-    };
-  }, []);
+//     return () => {
+//       window.removeEventListener("scroll", toggleVisibility);
+//     };
+//   }, []);
 
-  const scrollToTop = () => {
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth",
-    });
-  };
+//   const scrollToTop = () => {
+//     window.scrollTo({
+//       top: 0,
+//       behavior: "smooth",
+//     });
+//   };
 
-  return (
-    <button
-      onClick={scrollToTop}
-      class={`fixed bottom-6 right-6 p-3 rounded-xl shadow-lg bg-gray-600 text-white transition-opacity duration-300 hover:bg-yellow-400 cursor-pointer ${
-        isVisible ? "opacity-100" : "opacity-0 pointer-events-none"
-      }`}
-      aria-label="Volver arriba"
-    >
-      <span><ArrowTopIcon /></span>
-    </button>
-  );
-}
+//   return (
+//     <button
+//       onClick={scrollToTop}
+//       class={`fixed bottom-6 right-6 p-3 rounded-xl shadow-lg bg-gray-600 text-white transition-opacity duration-300 hover:bg-yellow-400 cursor-pointer ${
+//         isVisible ? "opacity-100" : "opacity-0 pointer-events-none"
+//       }`}
+//       aria-label="Volver arriba"
+//     >
+//       <span><ArrowTopIcon /></span>
+//     </button>
+//   );
+// }
