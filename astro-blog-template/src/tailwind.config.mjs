58639c58244b1ca88fec9b79186cc5ec175bec62
jonatan villalva <jvillalva.sistemas@gmail.com>
2026-01-28 19:22:01 -0300

    chore(tailwind): add typography plugin configuration

---
+++ astro-blog-template/src/tailwind.config.mjs
@@ -0,0 +1,9 @@
+import typography from "@tailwindcss/typography";
+
+export default {
+  content: ["./src/**/*.{astro,js,ts,md,mdx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [typography],
+};
